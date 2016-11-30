815528a2b0c5170c4dd50bc05bba3dcccb10c2fe
Sushi Yatsuhashi <sushi.yatsuhashi@gamil.com>
2016-11-30 20:40:19 +0900

    initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dep-nogi on 2016/11/30.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
---
+++ src/test/java/testTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by dep-nogi on 2016/11/30.
+ */
+public class testTest {
+
+}
